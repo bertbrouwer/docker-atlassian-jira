0e6dae3161a17c941fc7432add894425b177c83f
Dan Tehranian <dan.tehranian@delphix.com>
2015-04-22 16:18:13 -0700

    Consolidate the "sed" commands into a single command

--- launch.bash
+++ launch.bash
@@ -17,11 +17,14 @@ if [ -n "$DATABASE_URL" ]; then
   extract_database_url "$DATABASE_URL" DB /opt/jira/lib
   DB_JDBC_URL="$(xmlstarlet esc "$DB_JDBC_URL")"
 
-  sed -i.bak -e "s#<database-type>.*#<database-type>$DB_TYPE</database-type>#" /opt/atlassian-home/dbconfig.xml
-  sed -i.bak -e "s#<url>.*#<url>$DB_JDBC_URL</url>#" /opt/atlassian-home/dbconfig.xml
-  sed -i.bak -e "s#<driver-class>.*#<driver-class>$DB_JDBC_DRIVER</driver-class>#" /opt/atlassian-home/dbconfig.xml
-  sed -i.bak -e "s#<username>.*#<username>$DB_USER</username>#" /opt/atlassian-home/dbconfig.xml
-  sed -i.bak -e "s#<password>.*#<password>$DB_PASSWORD</password>#" /opt/atlassian-home/dbconfig.xml
+  sed \
+    -i.bak \
+    -e "s#<database-type>.*#<database-type>$DB_TYPE</database-type>#" \
+    -e "s#<url>.*#<url>$DB_JDBC_URL</url>#" \
+    -e "s#<driver-class>.*#<driver-class>$DB_JDBC_DRIVER</driver-class>#" \
+    -e "s#<username>.*#<username>$DB_USER</username>#" \
+    -e "s#<password>.*#<password>$DB_PASSWORD</password>#" \
+    /opt/atlassian-home/dbconfig.xml
 fi
 
 /opt/jira/bin/start-jira.sh -fg
